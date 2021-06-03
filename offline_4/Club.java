69b5839d880cc1324dedad4f9bb7832ecab79d0a
Mohaimin41 <1905041@ugrad.cse.buet.ac.bd>
2021-06-03 10:41:30 +0600

    Oh

--- offline_4/Club.java
+++ offline_4/Club.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Club {
+public class Club {
+    private int Id;
+    private String clubName;
+
+    public Club() {
+        Id = 0;
+        clubName = "";
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public void setName(String clubName) {
+        this.clubName = clubName;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public String getClubName() {
+        return clubName;
+    }
 }
